55455c8340c807cca1a60a5a7be82d623dd78bf3
pcarreira1 <p.carreira@outlook.pt>
2016-04-03 19:52:23 +0100

    image

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
 	var team=["01_AndrePires.gif","02_VitorTeixeira.gif","03_JoaoBandeira.gif","04LuisMagalhaes.gif","05_JoaoCosta.gif","06_NunoDuarte.gif","07_PedroCarreira.gif","08_JoaoSoares.gif","09_LilianaRibeiro.gif","10_DiogoGomes.gif","11_MiguelNunes.gif","12_RicardoLoureiro.gif","13_AnaAlves.gif","14_BeatrizCavaleiro.gif","15_AndrePeixoto.gif","16_MiguelSandim.gif","17_RicardoFigueiredo.gif","18_Matheus.gif","19_ClaudioMonteiro.gif","20_AndreBordalo.gif","21_JoaoCardoso.gif","22_CarolinaFaria.gif","23_Guilherme.gif","24_AndreRegado.gif","25_AnaRitaFerreira.gif","26_BrunoAlves.gif","27_JoaoPereira.gif","28_CarlosPereira.gif","29_JoseCardoso.gif","30_AnaísDias.gif","31_RaquelCorreia.gif","32_RitaLima.gif","33_DanielCouceiro.gif","34_JoaoFernandes.gif"];
 	
 	for(i=0;i<team.length;i++){
-		$("#teamImg").append('<button type="button" class="btn btn-default orange-circle-button blueCircle" style="background: url('+dir+team[i]+') no-repeat center;background-size: cover;"><div></div></button>');
+		$("#teamImg").append('<button type="button" class="btn btn-default orange-circle-button blueCircle" style="background: url('+dir+team[i]+') no-repeat center;background-size: cover;"><div class="hoverImage"></div></button>');
 	}
 
 
